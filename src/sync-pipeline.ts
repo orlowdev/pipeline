8a4b1ed25e185d6c62a6033c92c391690d748d24
Sergei Orlov <sergei_orlov@epam.com>
2019-08-07 16:31:31 +0300

    docs(sync pipeline): add annotations

--- src/sync-pipeline.ts
+++ src/sync-pipeline.ts
@@ -2,19 +2,50 @@ import { BasePipeline } from "./base-pipeline";
 import { MiddlewareInterface } from "./interfaces/middleware.interface";
 import { IntermediateInterface } from "./interfaces/intermediate.interface";
 
+/**
+ * Pipeline is an inverted Monoid that stores an array of middleware functions to be applied to data passed as
+ * pipeline.process() argument.
+ *
+ * Unlike Pipeline, SyncPipeline does not resolve Promises returned from previous Middleware function before passing
+ * it to the next one.
+ *
+ * **NOTE**: SyncPipeline creates a shallow copy of the context argument before passing it to the first middleware.
+ */
 export class SyncPipeline<TContext> extends BasePipeline<TContext> {
+  /**
+   * Pointer interface for lifting given middleware functions to a SyncPipeline.
+   * @param middleware - n Middleware functions.
+   */
   public static of<T>(...middleware: MiddlewareInterface<T>[]): SyncPipeline<T> {
     return new SyncPipeline<T>(middleware);
   }
 
+  /**
+   * Pointer interface for creating a SyncPipeline from array of Middleware.
+   * @param middleware - Array of Middleware functions.
+   */
   public static from<T>(middleware: MiddlewareInterface<T>[]): SyncPipeline<T> {
     return new SyncPipeline<T>(middleware);
   }
 
+  /**
+   * Pointer interface for creating an empty SyncPipeline.
+   */
   public static empty<T>(): SyncPipeline<T> {
     return new SyncPipeline<T>([]);
   }
 
+  /**
+   * Sequentially call middleware functions stored inside SyncPipeline starting with context provided as an
+   * argument.
+   *
+   * Values returned from middleware functions will be passed to the next middleware as an argument.
+   *
+   * If previous middleware function returned nullable value (**null** or **undefined**), the `ctx` will be
+   * passed to the next middleware unmodified.
+   *
+   * @param ctx
+   */
   public process(ctx: TContext): TContext {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     let result: IntermediateInterface | any = typeof ctx == "object" ? { ...ctx } : ctx;
