6219f0013661a1ce46ae4c6af1eb41ec680bd3c7
Sergei Orlov <sergei_orlov@epam.com>
2019-08-07 15:53:24 +0300

    docs(pipeline interface): add annotations

--- src/interfaces/pipeline.interface.ts
+++ src/interfaces/pipeline.interface.ts
@@ -1,10 +1,31 @@
 import { MiddlewareInterface } from "./middleware.interface";
 import { MonoidInterface, MonoidStaticInterface } from "./monoid.interface";
 
+/**
+ * Descriptor for Pipeline.
+ *
+ * @interface PipelineInterface
+ */
 export interface PipelineInterface<TContext = {}> extends MonoidInterface<TContext> {
+  /**
+   * Pipeline has no middleware flag.
+   */
   isEmpty: boolean;
+  /**
+   * Array of internally stored middleware.
+   */
   middleware: MiddlewareInterface<TContext>[];
+
+  /**
+   * Concat current middleware with argument pipeline of the same generic type.
+   * @param o
+   */
   concat(o: PipelineInterface<TContext>): PipelineInterface<TContext>;
+
+  /**
+   * Sequentially call middleware functions stored inside Pipeline starting with context provided as an argument.
+   * @param ctx
+   */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   process(ctx: TContext): any;
 }
