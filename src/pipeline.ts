8f53c64a4441a5338631cae247ee146518f9277d
Sergei Orlov <sergei_orlov@epam.com>
2019-08-07 16:24:35 +0300

    docs(pipeline): add annotations

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -2,19 +2,53 @@ import { BasePipeline } from "./base-pipeline";
 import { MiddlewareInterface } from "./interfaces/middleware.interface";
 import { IntermediateInterface } from "./interfaces/intermediate.interface";
 
+/**
+ * Pipeline is an inverted Monoid that stores an array of middleware functions to be applied to data passed as
+ * pipeline.process() argument.
+ *
+ * **NOTE**: Pipeline creates a shallow copy of the context argument before passing it to the first middleware.
+ */
 export class Pipeline<TContext> extends BasePipeline<TContext> {
+  /**
+   * Pointer interface for lifting given middleware functions to a IntermediatePipeline.
+   * @param middleware - n Middleware functions.
+   */
   public static of<T>(...middleware: MiddlewareInterface<T>[]): Pipeline<T> {
     return new Pipeline<T>(middleware);
   }
 
+  /**
+   * Pointer interface for creating a IntermediatePipeline from array of Middleware.
+   * @param middleware - Array of Middleware functions.
+   */
   public static from<T>(middleware: MiddlewareInterface<T>[]): Pipeline<T> {
     return new Pipeline<T>(middleware);
   }
 
+  /**
+   * Pointer interface for creating an empty IntermediatePipeline.
+   */
   public static empty<T>(): Pipeline<T> {
     return new Pipeline<T>([]);
   }
 
+  /**
+   * Sequentially call middleware functions stored inside Pipeline starting with context provided as an
+   * argument.
+   *
+   * Values returned from middleware functions will be passed to the next middleware as an argument.
+   *
+   * If previous middleware function returned nullable value (**null** or **undefined**), the `ctx` will be
+   * passed to the next middleware unmodified.
+   *
+   * If middleware that is currently being processed returns a Promise, it will be resolved before being passed to
+   * the next middleware.
+   *
+   * Returns Promise of contents of the transformed context. If error occurs during execution, it can be caught with
+   * returned Promise `.catch`.
+   *
+   * @param ctx
+   */
   public async process(ctx: TContext): Promise<TContext> {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     let result: IntermediateInterface | any = typeof ctx == "object" ? { ...ctx } : ctx;
