cd0da8bb33e21ac85d6a694b7f218dbbe6191857
priestine <priestine1.dev@gmail.com>
2020-03-24 22:30:25 +0300

    refactor: remove isIntermediate type guard
    BREAKING CHANGE: isIntermediate type guard is no longer available. Use Intermediate.isIntermediate instead

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,1 @@
 export * from "./intermediate";
-export * from "./is-intermediate";
--- src/helpers/is-intermediate.ts
+++
@@ -1,5 +0,0 @@
-import { IntermediateInterface } from "../interfaces";
-
-export function isIntermediate(x: unknown): x is IntermediateInterface {
-  return typeof x == "object" && x != null && "intermediate" in x;
-}
